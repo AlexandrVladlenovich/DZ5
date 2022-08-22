99832dc5c3c6c4ebbbc5919ed6b9a7d2917a2d49
AlexandrVladlenovich <Mystafin_Alexandr@mail.ru>
2022-08-22 23:11:47 +0500

    add Zadacha038_MassivRazniMinMax

---
+++ Zadacha038_MassivRazniMinMax/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+int[] array = { -2, 3, 4, 5, -6, 7 };
+int max = array[0];
+int min = array[0];
+
+for (int j = 0; j < array.Length; j++)
+{
+    if (array[j] > max) max = array[j];
+    if (array[j] < min) min = array[j];
+}
+Console.WriteLine();
+Console.Write("Разница между максимальным и минимальным элементом массива = ");
+Console.Write(max - min);
