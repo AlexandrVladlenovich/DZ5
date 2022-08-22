f5ac99eb0eb1b566e6cda24550a73ea622e0f5a1
AlexandrVladlenovich <Mystafin_Alexandr@mail.ru>
2022-08-22 22:46:06 +0500

    add Zadacha034_MassivTrehznachChisla

---
+++ Zadacha034_MassivTrehznachChisla/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+int[] array = new int[12];
+int countEven = 0;
+int countNotEven = 0;
+Console.Write("[ ");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 999);
+    Console.Write(array[i] + ", ");
+}
+Console.WriteLine("]");
+
+for (int j = 0; j < array.Length; j++)
+{
+    if (array[j] % 2 == 0) countEven = countEven + 1;
+    else countNotEven = countNotEven + 1;
+}
+Console.Write("Четные числа из массива: " + countEven);
+Console.WriteLine();
+Console.Write("Нечетные числа из массива: " + countNotEven);
